3950703deae65fe25f09854900bc4a47304399a0
Sam <samuelsahnixon@gmail.com>
2023-08-17 14:31:54 -0400

    Added Walls and Ceiling
    They are all different colors

--- Assets/Scripts/CreateLvl1.cs
+++ Assets/Scripts/CreateLvl1.cs
@@ -7,18 +7,40 @@ public class CreateLvl1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CubeModules moduleCreator = transform.GetComponent<CubeModules>(); 
-        moduleCreator.CreateFloorCubes(new Vector3(-10, -1, -10), 10, 10, transform.GetChild(0));
+        CubeModules moduleCreator = transform.GetComponent<CubeModules>();
+        var floorModule = transform.GetChild(0);
+        var wallModule = transform.GetChild(1);
+        var wallModule2 = transform.GetChild(2);
+        var wallModule3 = transform.GetChild(3);
+        var wallModule4 = transform.GetChild(4);
+        var ceilingModule = transform.GetChild(5);
+        moduleCreator.CreateFloorCubes(new Vector3(-10, -1, -10), 10, 10, floorModule);
+        moduleCreator.CreateWallCubes(new Vector3(-10, 0, -10), 10, 10, true, wallModule);
+        moduleCreator.CreateWallCubes(new Vector3(-10, 0, -10), 10, 10, false, wallModule2);
+        moduleCreator.CreateWallCubes(new Vector3(-10, 0, 12), 10, 10, true, wallModule3);
+        moduleCreator.CreateWallCubes(new Vector3(12, 0, -10), 10, 10, false, wallModule4);
+        moduleCreator.CreateFloorCubes(new Vector3(-10, 21, -10), 10 , 10, ceilingModule);
+        foreach (var modules in floorModule.GetComponentsInChildren<Renderer>())
+            modules.material.color = Color.red;
+        foreach (var modules in wallModule.GetComponentsInChildren<Renderer>())
+            modules.material.color = Color.blue;
+        foreach (var modules in wallModule2.GetComponentsInChildren<Renderer>())
+            modules.material.color = Color.yellow;
+        foreach (var modules in wallModule3.GetComponentsInChildren<Renderer>())
+            modules.material.color = Color.green;
+        foreach (var modules in wallModule4.GetComponentsInChildren<Renderer>())
+            modules.material.color = Color.black;
+        foreach (var modules in ceilingModule.GetComponentsInChildren<Renderer>())
+            modules.material.color = Color.white;
 
-        Transform floorModule = transform.GetChild(0);
-        for(int i = 0; i < floorModule.childCount; i++)
-        {
-            Transform child = floorModule.GetChild(i);
-            Debug.Log(child.name);
-            if (child.name.Equals("CubeModule(Clone)"))
-            {
-                child.GetComponent<Renderer>().material.color = Color.red;
-            }
-        }
+
+
+
+    }
+
+
+    Color GenerateRandomColor()
+    {
+        return new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0, 1f));
     }
 }
--- Assets/Scripts/CubeModules.cs
+++ Assets/Scripts/CubeModules.cs
@@ -8,12 +8,12 @@ public class CubeModules : MonoBehaviour
     public float cubeSize = 1f;
 
 
-    public ArrayList CreateFloorCubes(Vector3 startCorner, int xSize, int zSize, Transform parentGameObject)
+    public ArrayList CreateFloorCubes(Vector3 startCorner, int length, int width, Transform parentGameObject)
     {
         ArrayList floorCubes = new ArrayList();
-        for (int x = 0; x < xSize; x++)
+        for (int x = 0; x < length; x++)
         {
-            for (int z = 0; z < zSize; z++)
+            for (int z = 0; z < width; z++)
             {
                 GameObject cube = Instantiate(cubePrefab, parentGameObject);
                 cube.transform.position = new Vector3(startCorner.x + x * cubeSize + cubeSize / 2, startCorner.y, startCorner.z + z * cubeSize + cubeSize / 2);
@@ -22,4 +22,31 @@ public class CubeModules : MonoBehaviour
         }
         return floorCubes;
     }
+
+    public ArrayList CreateWallCubes(Vector3 startCorner, int length, int height,bool xDir,Transform parentGameObject)
+    {
+        int xOffset = 0;
+        int zOffset = 0;
+
+        if (xDir)
+            xOffset = 1;
+        else
+            zOffset = 1;
+        
+        ArrayList wallCubes = new ArrayList();
+        for (int x = 0; x < length; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                GameObject cube = Instantiate(cubePrefab, parentGameObject);
+                cube.transform.position = new Vector3(startCorner.x + x * cubeSize * xOffset - zOffset * cubeSize / 2 + xOffset * cubeSize / 2,
+                                                    startCorner.y + y * cubeSize + cubeSize / 2,
+                                                    startCorner.z + zOffset * cubeSize * x - xOffset * cubeSize / 2 + zOffset * cubeSize / 2);
+                cube.transform.rotation = Quaternion.Euler(0, 90, 0);
+                wallCubes.Add(cube);
+           }
+        }
+        return wallCubes;
+    }
+
 }
